72a58f1b2caa8e8c84bec1f04cb3a19348c66e8c
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-12-24 11:22:04 -0300

    HeaderParams

--- src/main/java/br/com/devlucassantos/main/controller/PrimeiraController.java
+++ src/main/java/br/com/devlucassantos/main/controller/PrimeiraController.java
@@ -1,16 +1,36 @@
-package br.com.devlucassantos.main;
+package br.com.devlucassantos.main.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/primeiraController")
 public class PrimeiraController {
 
-    @GetMapping("/primeiroMetodo")
-    public String primeiroMetodo(){
-        return "Meu primeiro metodo api rest";
+    @GetMapping("/primeiroMetodo/{id}")
+    public String primeiroMetodo(@PathVariable String id){
+        return "O parametro é " + id;
     }
 
+    @GetMapping("/metodoComQueryParams")
+    public String metodoComQueryParams(@RequestParam String id){
+        return "O parametro com metodoComQueryParams é" + id;
+    }
+
+    @GetMapping("/metodoComQueryParams2")
+    public String metodoComQueryParams2(@RequestParam Map<String, String> allParams){
+        return "O parametro com metodoComQueryParams2 é" + allParams.entrySet();
+    }
+
+    @PostMapping("/metodoComBodyParams")
+    public String meotodoComBodyParams(@RequestBody Usuario usuario ){
+        return "meotodoComBodyParams " + usuario.username();
+    }
+
+    @PostMapping("/metodoComHeaders")
+    public String meotodoComBodyParams(@RequestHeader ("name") String name){
+        return "metodoComHeaders " + name;
+    }
+    record Usuario(String username){}
 }
